83de3ed4b0f204dc7f3ce3bdb9eed193c28d4d16
FRANCO RODRIGUEZ PACIELLO <francorodriguez429@gmail.com>
2022-06-20 09:40:38 -0300

    Style and ternary operator

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,21 +1,44 @@
 import React,{Component} from "react";
 
+import './Task.css';
+
 class Task extends Component{
+
+styleCompleted (){
+    return {
+        fontSize: '20px',
+        color: this.props.task.done ? 'gray' : 'black',
+        textDecoration: 'none'
+    }
+};
+
     render() {
        const {task} =this.props;
 
-        return <div>
+        return <p style={this.styleCompleted()}>
             {task.title} - 
             {task.description} - 
             {task.done} - 
             {task.id}
         <input type ="checkbox"/>
-        <button>
+        <button style = {btnDelete}>
             x
         </button>
-        </div>
+        </p>
         
     }
 }
+
+
+const btnDelete ={
+    fontSize: '18px',
+    background:'#ea2027',
+    color: '#fff',
+    border:'none',
+    padding: '10px 15px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    
+};
 
 export default Task;
